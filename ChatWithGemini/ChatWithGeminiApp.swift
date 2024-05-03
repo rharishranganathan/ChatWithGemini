3fb3b3c1613fd1a691f59ce6fcc65e49b070a5eb
Etishagarg <gargetisha@gmail.com>
2024-05-03 14:55:48 +0530

    Initial Commit

---
+++ ChatWithGemini/ChatWithGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGeminiApp.swift
+//  ChatWithGemini
+//
+//  Created by Etisha Garg on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithGemini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatWithGemini
+//
+//  Created by Etisha Garg on 03/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
